f32fbdb61a3291fc17b335aa1e0fa39da7d37e52
swetha-kesaraju <122253724+swetha-kesaraju@users.noreply.github.com>
2023-01-11 10:25:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-uspnax
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-uspnax)
---
+++ src/components/userInputOne.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <label> <input type="checkbox" name="partyid" /> PartyId </label>
+  </div>
+</template>
+
+<script>
+export default {
+  return: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
